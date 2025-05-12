1347d402b628512701224c1f259c1cf9767dad17
Price Tula <pricetula@gmail.com>
2025-05-12 09:47:20 +0300

    add tailwind as package and use it on apps workspaces

---
+++ apps/public/tailwind.config.ts
@@ -0,0 +1,3 @@
+import config from '@somo-tracker-ui/tailwindcss/tailwind.config.js';
+
+export default config;
---
+++ apps/web/postcss.config.js
@@ -0,0 +1,3 @@
+import config from "@somo-tracker-ui/tailwindcss/postcss.config.js";
+
+export default config;
