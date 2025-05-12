661be7a9908d2acc3d3d1dd2c83e586aef91a808
Price Tula <pricetula@gmail.com>
2025-05-12 20:29:31 +0300

    added test libraries

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,12 +1,8 @@
-import { Button } from "@/components/ui/button"
+import { Dashboard } from "@/components/page/dashboard";
 
 
 export default function Home() {
     return (
-        <div>
-            <h1>Welcome to SvelteKit</h1>
-            <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-            <Button>Button</Button>
-        </div>
+        <Dashboard />
     );
 }
