e16e115863963e21401364cb325b6ebd7c224a3a
Price Tula <pricetula@gmail.com>
2025-05-12 10:42:18 +0300

    clean up

--- apps/public/src/routes/+page.svelte
+++ apps/public/src/routes/+page.svelte
@@ -2,6 +2,6 @@
 	import { Button } from '$lib/components/ui/button/index.js';
 </script>
 
-<h1 class="bg-red-300">Welcome to SvelteKit</h1>
+<h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<Button class="bg-red-400">Button</Button>
+<Button>Button</Button>
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
         <div>
             <h1>Welcome to SvelteKit</h1>
             <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-            <Button className="bg-red-400">Button</Button>
+            <Button>Button</Button>
         </div>
     );
 }
