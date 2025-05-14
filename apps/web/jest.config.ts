3b3669b5ce09778eb29f4d24ea21e11dda56fdf2
Price Tula <pricetula@gmail.com>
2025-05-14 10:04:31 +0300

    ignore e2e when jest is running

--- apps/web/jest.config.ts
+++ apps/web/jest.config.ts
@@ -10,6 +10,10 @@ const customJestConfig: Config.InitialOptions = {
     setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
     testEnvironment: 'jsdom',
     moduleDirectories: ['node_modules', '<rootDir>/'],
+    // Only look inside src/
+    testMatch: ['**/src/**/*.test.[jt]s?(x)'],
+    // Ignore the e2e tests
+    testPathIgnorePatterns: ['/node_modules/', '/tests/e2e/'],
 };
 
 export default createJestConfig(customJestConfig);
