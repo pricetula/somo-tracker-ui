009796f477a2b0d2899dd7a72a6741d3ac85ba3e
Price Tula <pricetula@gmail.com>
2025-05-14 15:13:46 +0300

    add auth config for better-auth

---
+++ apps/web/src/lib/auth.ts
@@ -0,0 +1,11 @@
+import { betterAuth } from "better-auth";
+import { Pool } from "pg";
+
+export const auth = betterAuth({
+    emailAndPassword: {
+        enabled: true
+    },
+    database: new Pool({
+        connectionString: process.env.BETTER_AUTH_DB_URL,
+    }),
+})
