4708c0849a1d7f74fed5a52fb656ba7e8df27db9
Price Tula <pricetula@gmail.com>
2025-05-20 02:51:25 +0300

    clean up

--- apps/web/src/app/signout/page.tsx
+++ apps/web/src/app/signout/page.tsx
@@ -1,6 +1,25 @@
 "use client"
 
+import { useEffect, useState } from "react"
+import { useRouter } from "next/navigation"
+import { createClient } from "@/lib/supabase/client"
+
 export default function Page() {
+    async function signOut() {
+        const router = useRouter()
+        const supabase = await createClient()
+        const { error } = await supabase.auth.signOut()
+        if (error) {
+            console.error("Error signing out:", error)
+            return
+        }
+        router.push("/")
+    }
+
+    useEffect(() => {
+        signOut()
+    }, [])
+
     return (
         <div>
             Signing out...
