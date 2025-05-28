70aad4155fb6b126727a94e74cd898f9c6ae33b7
Price Tula <pricetula@gmail.com>
2025-05-28 17:45:08 +0300

    clean up

--- apps/web/src/components/common/layout/Layout.tsx
+++ apps/web/src/components/common/layout/Layout.tsx
@@ -4,12 +4,11 @@ import { SiteHeader } from "./components/site-header"
 
 export function Layout({ children }: { children: React.ReactNode }) {
     return (
-
         <SidebarProvider>
             <AppSidebar />
             <SidebarInset>
                 <SiteHeader />
-                <div>
+                <div className="px-5">
                     {children}
                 </div>
             </SidebarInset>
