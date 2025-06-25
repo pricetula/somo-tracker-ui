6f91ad4547dd51b2b4d080337b32066eb5fd7f5c
Price Tula <pricetula@gmail.com>
2025-06-25 05:34:21 +0300

    clean up

--- apps/web/src/app/(dashboard)/layout.tsx
+++ apps/web/src/app/(dashboard)/layout.tsx
@@ -1,9 +1,10 @@
 import { Layout as AppLayout } from "@/components/common/layout"
 import { getAccessToken } from "@/lib/auth"
+import { getMe } from "@/lib/service/user"
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
     const token = await getAccessToken()
-    console.log("---------------------------------------->>>>", token)
+    const me = await getMe(token)
     return (
         <AppLayout>
             {children}
