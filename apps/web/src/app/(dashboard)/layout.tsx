78f92b456f8738788f8f43ef7654647818bf6c42
Price Tula <pricetula@gmail.com>
2025-06-25 12:30:01 +0300

    redirect to onboarding

--- apps/web/src/app/(dashboard)/layout.tsx
+++ apps/web/src/app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import { redirect } from "next/navigation"
 import { Layout as AppLayout } from "@/components/common/layout"
 import { getAccessToken } from "@/lib/auth"
 import { getMe } from "@/lib/service/user"
@@ -5,6 +6,9 @@ import { getMe } from "@/lib/service/user"
 export default async function Layout({ children }: { children: React.ReactNode }) {
     const token = await getAccessToken()
     const me = await getMe(token)
+    if (me?.error?.includes?.("sql: no rows in result set")) {
+        redirect("/onboarding")
+    }
     return (
         <AppLayout>
             {children}
