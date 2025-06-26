ae42e13f1cb29441ca9ddfaaf8213df715d45089
Price Tula <pricetula@gmail.com>
2025-06-26 14:30:02 +0300

    fix layout

--- apps/web/src/app/(public)/layout.tsx
+++ apps/web/src/app/(public)/layout.tsx
@@ -1,6 +1,9 @@
+import { Nav } from "@/shared/components/layout/nav/Nav";
+
 export default async function Layout({ children }: { children: React.ReactNode }) {
     return (
         <main className="container mx-auto px-4">
+            <Nav />
             {children}
         </main>
     )
