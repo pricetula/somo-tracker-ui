93c05867ce0a2d5d5104822ae7e2cea602fa624f
Price Tula <pricetula@gmail.com>
2025-07-17 22:59:59 +0300

    added pages

---
+++ apps/web/src/features/invitations/invitations-list/InvitationsList.tsx
@@ -0,0 +1,5 @@
+export async function InvitationsList() {
+    return (
+        <div>invitations</div>
+    )
+}
--- apps/web/src/shared/components/layout/dashboard-layout/components/site-header/SiteHeader.tsx
+++ apps/web/src/shared/components/layout/dashboard-layout/components/site-header/SiteHeader.tsx
@@ -1,8 +1,21 @@
+import React from "react"
+
+import { headers } from "next/headers"
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/shared/components/ui/breadcrumb"
 import { Separator } from "@/shared/components/ui/separator"
 import { SidebarTrigger } from "@/shared/components/ui/sidebar"
+import { parseBreadcrumbs } from "./utils"
 
-export function SiteHeader() {
+export async function SiteHeader() {
+    // get headers from request
+    const headersList = await headers();
+
+    // get the "x-pathname" field on request headers
+    const pathname = headersList.get("x-pathname") || "/"
+
+    // generate breadcrumb list of links
+    const breadCrumbs = parseBreadcrumbs(pathname)
+
     return (
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
             <div className="flex items-center gap-2 px-4">
@@ -10,15 +23,31 @@ export function SiteHeader() {
                 <Separator orientation="vertical" className="mr-2 h-4" />
                 <Breadcrumb>
                     <BreadcrumbList>
-                        <BreadcrumbItem className="hidden md:block">
-                            <BreadcrumbLink href="#">
-                                Building Your Application
-                            </BreadcrumbLink>
-                        </BreadcrumbItem>
-                        <BreadcrumbSeparator className="hidden md:block" />
-                        <BreadcrumbItem>
-                            <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                        </BreadcrumbItem>
+                        {
+                            breadCrumbs
+                                .map((breadcrumb) => (
+                                    <React.Fragment key={breadcrumb.id}>
+                                        <BreadcrumbItem>
+                                            {
+                                                breadcrumb.isLast
+                                                    ? (
+                                                        <BreadcrumbPage>{breadcrumb.label}</BreadcrumbPage>
+                                                    )
+                                                    : (
+                                                        <BreadcrumbLink href={breadcrumb.href}>
+                                                            {breadcrumb.label}
+                                                        </BreadcrumbLink>
+                                                    )
+                                            }
+                                        </BreadcrumbItem>
+                                        {
+                                            breadcrumb.isLast
+                                                ? null
+                                                : <BreadcrumbSeparator className="hidden md:block" />
+                                        }
+                                    </React.Fragment>
+                                ))
+                        }
                     </BreadcrumbList>
                 </Breadcrumb>
             </div>
