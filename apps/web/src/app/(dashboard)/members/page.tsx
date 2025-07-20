e11e4949775c70f55c5925a7a6e2b0c7b2fa0bac
Price Tula <pricetula@gmail.com>
2025-07-21 00:36:11 +0300

    add members table

--- apps/web/src/app/(dashboard)/members/page.tsx
+++ apps/web/src/app/(dashboard)/members/page.tsx
@@ -1,7 +1,3 @@
-export default function Page() {
-    return (
-        <article>
-            members
-        </article>
-    )
-}
+import { MembersList } from "@/features/members/members-list";
+
+export default MembersList
