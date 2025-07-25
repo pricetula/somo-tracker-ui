4af46ca08e9e6959098b67c3eba8891b5ce218da
Price Tula <pricetula@gmail.com>
2025-07-26 02:40:48 +0300

    add alert dialog

--- apps/web/src/features/institute-users/components/institute-user-list/InstituteUserListTable.tsx
+++ apps/web/src/features/institute-users/components/institute-user-list/InstituteUserListTable.tsx
@@ -3,29 +3,17 @@
 import * as React from "react"
 import { toast } from "sonner"
 import {
-    ColumnDef,
     flexRender,
     getCoreRowModel,
     getFilteredRowModel,
     getSortedRowModel,
     useReactTable,
 } from "@tanstack/react-table"
-import { Avatar, AvatarFallback, AvatarImage } from "@/shared/components/ui/avatar"
 import {
-    Edit,
-    Trash,
     ArrowDownCircle,
-    MoreHorizontal,
     Loader2Icon,
 } from "lucide-react"
 import { Button } from "@/shared/components/ui/button"
-import { Checkbox } from "@/shared/components/ui/checkbox"
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuTrigger,
-} from "@/shared/components/ui/dropdown-menu"
 import {
     Table,
     TableBody,
@@ -34,10 +22,11 @@ import {
     TableHeader,
     TableRow,
 } from "@/shared/components/ui/table"
-import { RoleSelector } from "@/shared/components/role-selector"
 import { BulkActions } from "@/shared/components/bulk-actions"
 import { GetInstituteUsersParams, GetInstituteUsersResponse, InstituteUsers } from "../../types"
 import { FilterButton } from "./FilterButton"
+import { ConfirmMemberRoleChange } from "./ConfirmMemberRoleChange"
+import { useInstituteUsersColumns } from "./hooks"
 
 interface InstituteUserListTableProps {
     instituteUsers: InstituteUsers[]
@@ -45,126 +34,7 @@ interface InstituteUserListTableProps {
 }
 
 export function InstituteUserListTable({ instituteUsers, getInstituteUsers }: InstituteUserListTableProps) {
-    const columns: ColumnDef<InstituteUsers>[] = React.useMemo(() => [
-        {
-            id: "select",
-            header: ({ table }) => (
-                <Checkbox
-                    checked={
-                        table.getIsAllPageRowsSelected() ||
-                        (table.getIsSomePageRowsSelected() && "indeterminate")
-                    }
-                    onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-                    aria-label="Select all"
-                />
-            ),
-            cell: ({ row }) => (
-                <Checkbox
-                    checked={row.getIsSelected()}
-                    onCheckedChange={(value) => row.toggleSelected(!!value)}
-                    aria-label="Select row"
-                />
-            ),
-            meta: {
-                className: "w-[20px] min-w-[20px] max-w-[20px]",
-            },
-            enableSorting: false,
-            enableHiding: false,
-        },
-        {
-            id: "name",
-            accessorFn: row => `${row.user.first_name || ""} ${row.user.last_name || ""}`,
-            header: "Name",
-            cell: ({ getValue, row }) => {
-                const name = getValue() as string
-                const abbreviatedName = name[0]
-                return (
-                    <div className="flex items-center gap-2">
-                        <Avatar className="h-8 w-8 rounded-lg">
-                            <AvatarImage src={row.original.user.photo_url} alt={name} />
-                            <AvatarFallback className="rounded-lg">{abbreviatedName}</AvatarFallback>
-                        </Avatar>
-                        <span>{name}</span>
-                    </div>
-                )
-            },
-            meta: {
-                className: "w-[400px]",
-            },
-        },
-        {
-            id: "email",
-            accessorFn: row => row.user.email || "",
-            header: "Email",
-            cell: ({ getValue }) => {
-                const email = getValue() as string
-                if (!email) return "-"
-                return (<a className="lowercase" href={`mailto:${email}`}>{email}</a>)
-            },
-            meta: {
-                className: "w-[300px]",
-            },
-        },
-        {
-            id: "phone",
-            accessorFn: row => row.user.phone || "",
-            header: "Phone",
-            cell: ({ getValue }) => (getValue() || "-") as string,
-            meta: {
-                className: "w-[300px]",
-            },
-        },
-        {
-            id: "role",
-            accessorFn: row => row.role || "",
-            header: () => (<span className="pl-4">Role</span>),
-            meta: {
-                className: "w-[80px]",
-            },
-            cell: ({ getValue }) => {
-                const role = (getValue()) as string
-                return (
-                    <RoleSelector
-                        id="role"
-                        value={role}
-                        onSetValue={console.log}
-                    />
-                )
-            },
-        },
-        {
-            id: "actions",
-            enableHiding: false,
-            cell: ({ row }) => {
-                const me = row.original
-
-                return (
-                    <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                            <Button variant="ghost" className="h-8 w-8 p-0">
-                                <span className="sr-only">Open menu</span>
-                                <MoreHorizontal />
-                            </Button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent align="end">
-                            <DropdownMenuItem>
-                                <Edit />
-                                <span>
-                                    Edit
-                                </span>
-                            </DropdownMenuItem>
-                            <DropdownMenuItem className="text-red-600 focus:bg-red-100 focus:text-red-600">
-                                <Trash />
-                                <span>
-                                    Delete
-                                </span>
-                            </DropdownMenuItem>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
-                )
-            },
-        },
-    ], [])
+    const columns = useInstituteUsersColumns(handleRequestUpdateInstituteUserRole)
     const [isSubmitting, setIsSubmitting] = React.useState(false)
     const [isNoMoreUsers, setIsNoMoreUsers] = React.useState(false)
     const [roles, setRoles] = React.useState<string[]>([])
@@ -176,7 +46,7 @@ export function InstituteUserListTable({ instituteUsers, getInstituteUsers }: In
     }, [roles, fetchedInstituteUsers])
 
     const table = useReactTable({
-        columns,
+        columns: columns(),
         data: filteredInstituteUsers,
         getCoreRowModel: getCoreRowModel(),
         getSortedRowModel: getSortedRowModel(),
@@ -209,6 +79,10 @@ export function InstituteUserListTable({ instituteUsers, getInstituteUsers }: In
             return
         }
         setFetchedInstituteUsers((prev) => [...prev, ...data])
+    }
+
+    function handleRequestUpdateInstituteUserRole(instituteUsers: InstituteUsers) {
+        console.log(instituteUsers)
     }
 
     return (
@@ -297,6 +171,7 @@ export function InstituteUserListTable({ instituteUsers, getInstituteUsers }: In
             <BulkActions
                 open={!!Object.keys(rowSelection).length}
             >sss</BulkActions>
+            <ConfirmMemberRoleChange />
         </article>
     )
 }
