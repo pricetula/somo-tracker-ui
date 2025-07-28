edba4db82441852fb9d6b47d1a48d21248818dc3
Price Tula <pricetula@gmail.com>
2025-07-28 23:32:47 +0300

    clean up

--- apps/web/src/app/(dashboard)/page.tsx
+++ apps/web/src/app/(dashboard)/page.tsx
@@ -1,21 +1,8 @@
-import { Button } from "@somo-tracker-ui/ui/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@somo-tracker-ui/ui/card";
 
 export default function Home() {
     return (
         <div className="flex justify-center items-center h-full">
-            <Card className="w-1/2">
-                <CardHeader>
-                    <CardTitle>Welcome to the Dashboard</CardTitle>
-                    <CardDescription>This is a test of the new dark theme.</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <p>The components should now be themed correctly.</p>
-                </CardContent>
-                <CardFooter>
-                    <Button>Click me</Button>
-                </CardFooter>
-            </Card>
+            home
         </div>
     )
 }
