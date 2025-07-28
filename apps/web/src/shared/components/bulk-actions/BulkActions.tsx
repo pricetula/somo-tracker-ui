8d082fa0c8ca141823fb2fa66f34c0123ce32ab7
Price Tula <pricetula@gmail.com>
2025-07-28 08:29:33 +0300

    fix buulk actions

--- apps/web/src/shared/components/bulk-actions/BulkActions.tsx
+++ apps/web/src/shared/components/bulk-actions/BulkActions.tsx
@@ -12,7 +12,7 @@ export function BulkActions({ open, children }: BulkActionsProps) {
         <div
             className={`fixed bottom-0 left-0 right-0 mx-auto h-20 w-max transform transition-all duration-200 ease-in-out ${open ? 'visible -translate-y-16' : 'invisible translate-y-16'}`}
         >
-            <Card className="flex h-16 items-center justify-center bg-background p-5 shadow-2xl">
+            <Card className="flex h-16 items-center justify-center bg-background p-5 rounded-lg shadow-md">
                 {children}
             </Card>
         </div>
