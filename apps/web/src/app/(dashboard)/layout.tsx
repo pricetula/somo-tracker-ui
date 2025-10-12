ffe3c2a819bf2c1569a81541398a6caeed64e23c
Price Tula <pricetula@gmail.com>
2025-10-12 14:26:55 +0300

    clean up

--- apps/web/src/app/(dashboard)/layout.tsx
+++ apps/web/src/app/(dashboard)/layout.tsx
@@ -1,9 +1,9 @@
 import { redirect } from "next/navigation"
 import { DehydratedState, HydrationBoundary, dehydrate } from "@tanstack/react-query";
-import { getMe } from "@/features/me/services/get-me"
 import { DashboardLayout } from "@/shared/components/layout/dashboard-layout"
 import { TokenRefreshFailedError } from "@/features/auth/errors"
 import { makeQueryClient } from "@/shared/lib/query-client"
+import { meQuery } from "@/features/me/queries/config";
 
 // This layout is used for the dashboard and requires the user to be logged in
 export default async function Layout({ children, modal }: { modal: React.ReactNode, children: React.ReactNode }) {
@@ -12,10 +12,7 @@ export default async function Layout({ children, modal }: { modal: React.ReactNo
 
     try {
         // Prefetch user
-        const me = await queryClient.fetchQuery({
-            queryKey: ['me'],
-            queryFn: getMe,
-        });
+        const me = await queryClient.fetchQuery(meQuery);
 
         if (!me) {
             redirect("/signout")
