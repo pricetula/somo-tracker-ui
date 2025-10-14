7835dc0924320ea94fc3307da3b3a9f8237cd5d1
Price Tula <pricetula@gmail.com>
2025-10-14 14:42:48 +0300

    added new component to create institute and home school

---
+++ apps/web/src/app/onboarding/create-home-school/page.tsx
@@ -0,0 +1,7 @@
+import { CreateHomeSchoolForm } from "@/features/institutes/components/create-home-school";
+
+export default function Page() {
+    return (
+        <CreateHomeSchoolForm />
+    );
+}
--- apps/web/src/features/onboarding/components/choose-institute-type/ChooseInstituteType.tsx
+++ apps/web/src/features/onboarding/components/choose-institute-type/ChooseInstituteType.tsx
@@ -1,5 +1,35 @@
+
+import Link from "next/link";
+import { Button } from "@/shared/components/ui/button";
+
 export function ChooseInstituteType() {
     return (
-        <article></article>
+        <div className="h-4/5 flex items-center justify-center">
+            <article>
+                <h1 className="text-center">Choose Institute Type</h1>
+                <section>
+                    <ul className="flex gap-4">
+                        <li className="w-1/2 shrink-0 bg-red-300">
+                            <h2>Learning Institute</h2>
+                            <p>This is for a school with children enrolled</p>
+                            <Button asChild>
+                                <Link href="/onboarding/create-institute">
+                                    Create Institute
+                                </Link>
+                            </Button>
+                        </li>
+                        <li className="w-1/2 shrink-0 bg-red-300">
+                            <h2>Home school</h2>
+                            <p>This is for home school geared towards parents and their children</p>
+                            <Button asChild>
+                                <Link href="/onboarding/create-home-school">
+                                    Create Home School
+                                </Link>
+                            </Button>
+                        </li>
+                    </ul>
+                </section>
+            </article>
+        </div>
     )
 }
