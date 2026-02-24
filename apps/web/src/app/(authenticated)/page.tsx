5fa05abb58f3f69a53a8ff12ee23215f7d7a8482
Price Tula <pricetula@gmail.com>
2026-02-24 14:52:49 +0300

    clean up

--- apps/web/src/app/(authenticated)/page.tsx
+++ apps/web/src/app/(authenticated)/page.tsx
@@ -3,11 +3,9 @@
 import { useRouter } from "next/navigation";
 
 export default function Page() {
-    const router = useRouter();
-
-    const t = () => {
-        router.replace("/onboarding");
-    };
-
-    return <button onClick={t}>dd</button>;
+    return (
+        <div>
+            dashboard
+        </div>
+    )
 }
