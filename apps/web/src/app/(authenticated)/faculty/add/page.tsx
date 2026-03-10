2a688642ccc759c7c0efd7741a6a9231ecfbe136
Price Tula <pricetula@gmail.com>
2026-03-10 22:17:35 +0300

    add ability to import faculty

---
+++ apps/web/src/app/(authenticated)/faculty/add/page.tsx
@@ -0,0 +1,11 @@
+import { CsvImporter } from "@/components/importer/csv-importer";
+import { bulkAddFaculty } from "@/features/faculty/api/actions";
+
+export default function AddFacultyPage() {
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-2xl font-semibold">Add faculty</h1>
+      <CsvImporter onImport={bulkAddFaculty} />
+    </div>
+  );
+}
